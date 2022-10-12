70c19f980cdb199896adbb8f1f4ec7fd8e1d16d0
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-10-12 13:02:08 +0300

    edit-usrer-profile

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from cProfile import label
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Profile
 class customUserCreationForm(UserCreationForm):
     class Meta:
         model = User
@@ -13,4 +13,20 @@ class customUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(customUserCreationForm, self).__init__(*args, **kwargs)
         for name, feild in self.fields.items():
-            feild.widget.attrs.update({'class': 'input'})        
+            feild.widget.attrs.update({'class': 'input'})   
+             
+      
+
+class Profileform(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'email', 'username',
+                  'location', 'bio', 'short_intro', 'profile_image',
+                  'social_github', 'social_linkedin', 'social_twitter',
+                  ]
+
+    def __init__(self, *args, **kwargs):
+        super(Profileform, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})           
--- users/views.py
+++ users/views.py
@@ -4,15 +4,16 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import customUserCreationForm
+from .forms import customUserCreationForm,Profileform
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 # Create your views here.
-
+@login_required(login_url='login')  
 def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
+@login_required(login_url='login')      
 def userprofile(request,pk):
     profiles = Profile.objects.get(id=pk)
 
@@ -53,21 +54,24 @@ def registerUser(request):
     page = 'register'
     form = customUserCreationForm()
 
-
     if request.method == 'POST':
         form = customUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            messages.success(request,'user account was created')
 
-            login(request,user)
-            return redirect('profiles')
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('edit-account')
+
         else:
-             messages.success(request,'an error occured during registration')   
-    context = {'page': page, 'form':form}
-    return render(request ,'users/login_register.html', context)   
+            messages.success(
+                request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)   
 
 @login_required(login_url='login')        
 def userAccount(request):
@@ -78,7 +82,18 @@ def userAccount(request):
     
     context = {'profile':profile, 'skills':skills,'projects':projects }
     return render(request, 'users/account.html', context)
-@login_required(login_url='login')     
+@login_required(login_url='login')
 def editAccount(request):
-    context={}
-    return render(request, 'users/profile_form.html', context)    
+    profile = request.user.profile
+    form = Profileform(instance=profile)
+
+    if request.method == 'POST':
+        form = Profileform(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
+  
